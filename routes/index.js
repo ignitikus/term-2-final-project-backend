0b254a5dccb79455c0b5c73adac7a75a54bf570f
NikolayK <nikolay.kim@codeimmersive.com>
2020-04-16 23:59:10 -0400

    adds test path with test message

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/info', function(req, res, next) {
+  return res.json({message: 'Coming from backend2'})
 });
 
 module.exports = router;
